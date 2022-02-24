bfd4999941ad2cfde00671217edfb30ec2ebee56
devexperience <devexperience@mx.com>
2022-02-24 22:54:27 +0000

    Generated version 0.7.1
    This pull request was automatically generated by a GitHub Action to generate version 0.7.1 of this library.

--- docs/MxPlatformApi.md
+++ docs/MxPlatformApi.md
@@ -1019,7 +1019,7 @@ Name | Type | Description  | Notes
 
 Create user
 
-Call this endpoint to create a new user. The MX Platform API will respond with the newly-created user object if successful. This endpoint accepts several parameters - id, metadata, and is_disabled. Disabling a user means that accounts and transactions associated with it will not be updated in the background by MX. It will also restrict access to that users data until they are no longer disabled. Users who are disabled for the entirety of an MX Platform API billing period will not be factored into that months bill.
+Use this endpoint to create a new user. The API will respond with the newly-created user object if successful. Disabling a user means that accounts and transactions associated with it will not be updated in the background by MX. It will also restrict access to that user’s data until they are no longer disabled.
 
 ### Example
 
@@ -7967,7 +7967,7 @@ Name | Type | Description  | Notes
 
 Update user
 
-Use this endpoint to update the attributes of a specific user. The MX Platform API will respond with the updated user object. Disabling a user means that accounts and transactions associated with it will not be updated in the background by MX. It will also restrict access to that users data until they are no longer disabled. Users who are disabled for the entirety of an MX Platform API billing period will not be factored into that months bill. To disable a user, update it and set the is_disabled parameter to true. Set it to false to re-enable the user.
+Use this endpoint to update the attributes of the specified user.
 
 ### Example
 
--- mx_platform_python/api/mx_platform_api.py
+++ mx_platform_python/api/mx_platform_api.py
@@ -6550,7 +6550,7 @@ class MxPlatformApi(object):
     ):
         """Create user  # noqa: E501
 
-        Call this endpoint to create a new user. The MX Platform API will respond with the newly-created user object if successful. This endpoint accepts several parameters - id, metadata, and is_disabled. Disabling a user means that accounts and transactions associated with it will not be updated in the background by MX. It will also restrict access to that users data until they are no longer disabled. Users who are disabled for the entirety of an MX Platform API billing period will not be factored into that months bill.  # noqa: E501
+        Use this endpoint to create a new user. The API will respond with the newly-created user object if successful. Disabling a user means that accounts and transactions associated with it will not be updated in the background by MX. It will also restrict access to that user’s data until they are no longer disabled.  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
 
@@ -13140,7 +13140,7 @@ class MxPlatformApi(object):
     ):
         """Update user  # noqa: E501
 
-        Use this endpoint to update the attributes of a specific user. The MX Platform API will respond with the updated user object. Disabling a user means that accounts and transactions associated with it will not be updated in the background by MX. It will also restrict access to that users data until they are no longer disabled. Users who are disabled for the entirety of an MX Platform API billing period will not be factored into that months bill. To disable a user, update it and set the is_disabled parameter to true. Set it to false to re-enable the user.  # noqa: E501
+        Use this endpoint to update the attributes of the specified user.  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
 
