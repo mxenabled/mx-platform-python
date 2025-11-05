aee6147a5fc1989ed7c2a47e3c637f991e573a85
devexperience <devexperience@mx.com>
2025-11-05 20:39:57 +0000

    Generated version 1.9.0
    This commit was automatically created by a GitHub Action to generate version 1.9.0 of this library.

--- mx_platform_python/configuration.py
+++ mx_platform_python/configuration.py
@@ -392,7 +392,7 @@ conf = mx_platform_python.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.1.0\n"\
-               "SDK Package Version: 1.8.0".\
+               "SDK Package Version: 1.9.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
